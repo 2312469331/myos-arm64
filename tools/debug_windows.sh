8e1483cb88ea21af904b57477f81a78b92a78509
liuweiji <2312469331@qq.com>
2026-04-23 16:48:03 +0800

    修复windows中文乱码

--- tools/debug_windows.sh
+++ tools/debug_windows.sh
@@ -2,8 +2,5 @@
 # 后台新终端运行 make debug
 # Windows Git Bash 专用
 
-# 方法1：start 独立 bash（最稳）
-start bash -c "make debug; exec bash"
-
-# 方法2：如果上面不行，用这个
-# start /MIN bash -c "make debug; exec bash"
+# 完美：不闪退 + 中文不乱码
+start bash -c "chcp.com 65001; make debug; exec bash"
